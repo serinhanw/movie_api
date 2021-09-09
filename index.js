7ee0b4472379b846920fdf5f113169286ad43977
serinhanw <79671093+serinhanw@users.noreply.github.com>
2021-09-09 04:36:29 -0700

    initiate mongodb atlas

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const Users = Models.User;
 const Directors = Models.Director;
 const Genres = Models.Genre;
 
-mongoose.connect('mongodb://localhost:27017/StubzDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+// mongoose.connect('mongodb://localhost:27017/StubzDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+
 
 const cors = require('cors');
 app.use(cors());

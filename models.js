2c279f2f6516d956b1fd6aea01b7cf6e700d45dd
serinhanw <79671093+serinhanw@users.noreply.github.com>
2021-09-09 01:52:48 -0700

    integrated bcrypt, hash

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
@@ -18,6 +19,14 @@ let userSchema = mongoose.Schema({
   favorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}],
   Birthday: Date
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 let directorSchema = mongoose.Schema({
   Name: {type: String, required: true},

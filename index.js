60b7aaf4a7d15db4dcef06d47a98ec7b0fda1ac8
serinhanw <79671093+serinhanw@users.noreply.github.com>
2021-10-12 00:33:56 -0700

    test update

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: true })); //false
 
 // ___CORS___
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://stubz.herokuapp.com', '*'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://stubz.herokuapp.com'/*, '*'*/];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
@@ -73,7 +73,7 @@ app.get('/', (req, res) => {
 });
 
 // _____get all movies_____
-app.get('/movies', /*passport.authenticate('jwt', {session: false}),*/ (req, res) => {
+app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
   Movies.find().populate('Genre Director')
   .then((movies) => {
     res.status(201).json(movies);
